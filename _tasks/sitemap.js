11dd9e06e8fbfd7aff4e2630e0ce6b544e2245cd
Yann Eves <hello@yanneves.com>
2017-02-24 01:11:15 +0000

    Updated sitemap to read from site.json instead of package.json

--- _tasks/sitemap.js
+++ _tasks/sitemap.js
@@ -3,12 +3,12 @@
 const path = require('path')
 const gulp = require('gulp')
 const gulpSitemap = require('gulp-sitemap')
-const { homepage } = require('../package.json')
+const { url } = require('../site.json')
 const { dist } = require('../paths.json')
 
 module.exports = function sitemap() {
 
   return gulp.src(path.join(dist.dir, '**/*.html'))
-    .pipe(gulpSitemap({ siteUrl: homepage }))
+    .pipe(gulpSitemap({ siteUrl: url }))
     .pipe(gulp.dest(dist.dir))
 }
