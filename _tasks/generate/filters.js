826f5d6d677a2365bc080cc5b2fc20d41bf7d30e
Yann Eves <hello@yanneves.com>
2017-04-07 20:27:04 +0100

    Removed test overrides in date filters

--- _tasks/generate/filters.js
+++ _tasks/generate/filters.js
@@ -5,17 +5,17 @@ const moment = require('moment')
 // Recent news headlines
 module.exports.headlines = ({ collection, date }) => (
   collection.includes('news') &&
-  moment(date).isSameOrAfter(moment('2013-05-20').subtract(2, 'months'))
+  moment(date).isSameOrAfter(moment().subtract(2, 'months'))
 )
 
 // Upcoming events
 module.exports.upcoming = ({ collection, date }) => (
   collection.includes('events') &&
-  moment(date).isSameOrAfter(moment('2016-01-20'), 'day')
+  moment(date).isSameOrAfter(moment(), 'day')
 )
 
 // Past events
 module.exports.past = ({ collection, date }) => (
   collection.includes('events') &&
-  moment(date).isBefore(moment('2016-01-20'), 'day')
+  moment(date).isBefore(moment(), 'day')
 )
