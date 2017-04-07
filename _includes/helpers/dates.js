ca2a0058ecff47deb4ce9e5172befd2c4f37398c
Yann Eves <hello@yanneves.com>
2017-04-07 21:40:32 +0100

    Fixed formatting of dates to always use UTC timezone

--- _includes/helpers/dates.js
+++ _includes/helpers/dates.js
@@ -2,6 +2,6 @@
 
 const moment = require('moment')
 
-module.exports.timestamp = date => moment(date).format()
-module.exports.day = date => moment(date).format('dddd, DD MMMM YYYY')
-module.exports.time = date => moment(date).format('h:mma')
+module.exports.timestamp = date => moment.utc(date, 'YYYY-MM-DDTHH:mm:ss.SSSZ').format()
+module.exports.day = date => moment.utc(date, 'YYYY-MM-DDTHH:mm:ss.SSSZ').format('dddd, Do MMMM YYYY')
+module.exports.time = date => moment.utc(date, 'YYYY-MM-DDTHH:mm:ss.SSSZ').format('h:mma')
