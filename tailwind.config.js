a52f8cac60ee649cc4919d044eeef9fa5db0701a
Yann イーベス Eves <hello@yanneves.com>
2025-02-06 21:43:03 +0000

    Upgraded to Tailwind v4

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./_{assets,includes,layouts,pages,posts}/**/*.{html,md,hbs,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
